cc13e2fa29cda14903fc49f0c14c9fe0997e89d1
AtomTech <elev200806@gmail.com>
2026-07-07 18:31:03 +0530

    Css_StylesFolder_Export-Import Case

---
+++ cssStyling/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/Header.css'
+
+const Header = ()=>{
+    return(
+        <div className="header">
+            <h3>Lakshya Gaur</h3>
+            <button>Log In</button>
+        </div>
+
+    )
+
+}
+
+
+export default Header;
