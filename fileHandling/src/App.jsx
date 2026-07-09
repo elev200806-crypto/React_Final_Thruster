8e69e5c5ffd9a8cf303da52093bbe25d06c228d9
AtomTech <elev200806@gmail.com>
2026-07-09 05:55:39 +0530

    Form Handling Done

---
+++ fileHandling/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const App = ()=>{
+
+  // form handling
+
+  const submitForm = (e)=>{
+    e.preventDefault()
+    console.log("Form Submitted")
+
+  }
+
+
+  return(
+    <div>
+      <form onSubmit={(a)=>{
+          submitForm(a)
+
+        }}>
+        <input type="text" placeholder="Enter your name" />
+        <button>Submit</button>
+        
+      </form>
+    </div>
+
+  )
+
+
+}
+
+
+export default App;
