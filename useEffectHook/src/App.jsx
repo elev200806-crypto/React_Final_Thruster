f18657784fbd303ce91c1f5c78ab800ebd1b04de
AtomTech <elev200806@gmail.com>
2026-07-10 13:09:20 +0530

    useEffect Hook Dependency Concept Done

--- useEffectHook/src/App.jsx
+++ useEffectHook/src/App.jsx
@@ -2,40 +2,51 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState(0)
-  const [num2, setNum2] = useState(100)
+  const [a,setA] = useState(0)
+  const [b,setB] = useState(0)
+  
+  function meow1(){
+    console.log("A changed")
 
-  useEffect(function () {
-    console.log("useEffect is Running")
+  }
+  function meow2(){
+    console.log("B changed")
 
-  }, [num])
+  }
+  
+  useEffect(function(){
+    meow1()
+    
 
-  // },[])    //this would run only one time in allover the Rendering!
-  // here the [] is dependencies
+  },[a])
+  useEffect(function(){
+    meow2()
 
-
+  },[b])
   // useEffect(function(){
-  //   console.log("useEffect is Running")
+  //   console.log("I am On.....")
 
-  // })  This code will run everytime when ever the console has UI change in the code  
+  // },[a,b])
 
+  
   return (
     <div>
-      <h1>Value of Num : {num}</h1>
-      <h1>Value of Num2 : {num2}</h1>
-      <button onMouseEnter={() => {
-        setNum(num + 1)
+        <h1>A is {a}</h1>
+        <h1>B is {b}</h1>
+        <button onClick={()=>{
+          setA(a+1)
+        }}>Change A</button>
+        <button onClick={()=>{
+          setB(b-1)
+
+        } }>Change B</button>
 
 
-      }}
-
-        onMouseLeave={() => {
-          setNum2(num2 + 10)
-
-        }}>Click Me</button>
     </div>
 
   )
+  
+  
 
 }
 
