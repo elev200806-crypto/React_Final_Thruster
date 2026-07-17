21f257c36d4abd87d0011a2212de915ea3a4eb71
AtomTech <elev200806@gmail.com>
2026-07-17 08:52:21 +0530

    useEffect HOOK Smoohly Cleared

--- reactRouterAdv/src/App.jsx
+++ reactRouterAdv/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
 import Home from "../../reactRouterDOM/src/pages/home";
 import About from "../../reactRouterDOM/src/pages/about";
@@ -9,10 +9,29 @@ import Navbar from "./components/Navbar";
 
 const App = ()=>{
 
+  const [num,setNum] = useState(0)
+
+  useEffect(()=>{
+    console.log("Rendering Successfully!")
+
+  },[])
   
   return(
     <>
       
+    <div>
+
+      <h1>{num}</h1>
+      <button onClick={()=>{
+        setNum(num + 1)
+      } }>Increase</button><br /><br />
+      <button onClick={()=>{
+        setNum(num - 1)
+      } }>Decrease</button>
+    </div>
+
+
+
       <Router>
           <Navbar/>
         <Routes>
