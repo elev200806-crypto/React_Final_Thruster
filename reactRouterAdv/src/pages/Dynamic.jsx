3e2ee4821baa7906a08664f5feee7d87fd5458a6
AtomTech <elev200806@gmail.com>
2026-07-18 12:18:12 +0530

    Advanced useParams

--- reactRouterAdv/src/pages/Dynamic.jsx
+++ reactRouterAdv/src/pages/Dynamic.jsx
@@ -5,10 +5,24 @@ const Dynamic = ()=>{
     // console.log(useParams())
 
     const {id} = useParams()
+    const users = [
+        { id: 1, name: "Aarav", age: 22, city: "Delhi" },
+        { id: 2, name: "Priya", age: 25, city: "Mumbai" },
+        { id: 3, name: "Rohan", age: 20, city: "Bangalore" },
+        { id: 4, name: "Neha", age: 27, city: "Jaipur" },
+        { id: 5, name: "Kabir", age: 24, city: "Pune" }
+    ];
+
+    const spec = users.find(data=> data.id == id)
+    
 
     return(
         <>
-            <h1>Id : {id}</h1>
+            <div>
+                <h1>Name : {spec.name}</h1>
+                <h1>Age : {spec.id}</h1>
+                <h1>City : {spec.city}</h1>
+            </div>
         </>
 
     )
